aa0ab5daf1a131fd7d711d85c842997d14dea3fc
Octane CI <octane@phase2technology.com>
2021-11-29 14:32:47 -0500

    Add generator script

--- generate.sh
+++ generate.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 # Generate a new project from Octane
+# Usage: npx github:phase2/octane-update [tag]
+#   tag: optional tag or commit hash of generator.
 echo "Hello world"
+octaneFolder=".octane-ci"
+tmpFolder="_octane"
+rm -rf "$tmpFolder"
+git clone git@github.com:phase2/octane-ci.git "$tmpFolder"
+rsync --delete -ac "$tmpFolder/.octane-ci/" "$octaneFolder/"
+rm -rf "$tmpFolder"
